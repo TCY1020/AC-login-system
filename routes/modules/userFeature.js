a18b4f7dc2bfaa6e848c85a51ef56e79217fe1dd
TCY1020 <a25023423@gmail.com>
2023-05-22 18:31:14 +0800

    feat: add login Path

--- routes/modules/userFeature.js
+++ routes/modules/userFeature.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const user = require('../../models/user.js')
 
+let key 
 
 //比對帳密
 //正確----->導入頁面
@@ -15,14 +16,24 @@ router.post('/check', (req, res) => {
     .lean()
     .then((userCheck) => {
       if (userCheck) {
-        res.render('userPage', { userCheck })
+        //res.render('userPage', { userCheck })
+        res.redirect('/profile/login')
+        key = userCheck._id
       } else {
         res.render('index', { wrong:'抱歉，找不到與此電子郵件地址相關的帳戶。請再試一次'})
       }
     })
     .catch(error => console.log(error))
+    //console.log(key)
+    
 })
 
+router.get('/login', (req, res) => {
+  user.findById(key._id)
+  .lean()
+    .then(userData => res.render('userPage', { userData }))
+  
+})
 
 
 module.exports = router
